0160e169377614e1351809f8978ab3567bdae65d
karkyon <karkyon@gmail.com>
2026-05-06 16:37:20 +0900

    fix: CustomDocEditor path, remove Downloads folder

--- Downloads/src__components__custom-docs__CustomDocEditor.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-
-export { DocumentEditor as CustomDocEditor } from "../documents/DocumentEditor";
-export { DocumentEditor as default } from "../documents/DocumentEditor";
--- src/components/custom-docs/CustomDocEditor.tsx
+++ src/components/custom-docs/CustomDocEditor.tsx
@@ -1,4 +1,4 @@
 "use client";
 
-export { DocumentEditor as CustomDocEditor } from "../documents/DocumentEditor";
-export { DocumentEditor as default } from "../documents/DocumentEditor";
+export { DocumentEditor as CustomDocEditor } from "@/components/documents/DocumentEditor";
+export { DocumentEditor as default } from "@/components/documents/DocumentEditor";
